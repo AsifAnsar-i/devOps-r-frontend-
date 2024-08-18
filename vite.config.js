22870bef040cd1138b9c0ad152e6ff46d35870c6
Asif Ansari <moaisfansari1999@gmail.com>
2024-08-18 15:45:32 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:7000",
+        target: "https://backend-devops-r.onrender.com",
         secure: false,
       },
     },
